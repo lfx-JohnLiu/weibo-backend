e413971956362b0483b31b50ca54cca8cc9b5dbf
LC <liucong@bjx.cloud>
2020-12-07 20:11:59 +0800

    feat: 调整目录结构

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-~ koa-genarator # koa cli
+koa-genarator # koa cli
+cross-env # 用于设置环境变量
--- src/app.js
+++ src/app.js
@@ -15,9 +15,9 @@ onerror(app)
 // middlewares
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
-}))
+})) // post 时解析 json 等
 app.use(json())
-app.use(logger())
+app.use(logger()) // log
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
@@ -25,12 +25,12 @@ app.use(views(__dirname + '/views', {
 }))
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
 app.use(index.routes(), index.allowedMethods())
